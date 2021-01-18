68468e1ea2c68a591e0efafebf344be9078c3f4d
Drantho <drantho@gmail.com>
2021-01-18 01:03:36 -0800

    department functions refactored as asyncc

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mainQuestions = [
         "name": "action",
         "type": "list",
         "message": "What would you like to do?",
-        "choices": ["Add Department", "Quit"]
+        "choices": ["Add Department", "View Departments", "Quit"]
     }
 ]
 
@@ -19,25 +19,40 @@ const addDepartmentquestions = [
 ]
 
 const main = () => { 
+
     inquirer.prompt(mainQuestions).then(response => {
         switch(response.action){
             case "Add Department":
                 addDepartment();                
                 break;
-            default :
+            case "View Departments" :
+                viewDepartments();
                 break;
+            default :
+                return;
         }
     })
 }
 
 main();
 
-const addDepartment = () =>{
+const addDepartment = async () =>{
 
-    inquirer.prompt(addDepartmentquestions).then(response => {
-        const department = new Department(response.name);
-    
-        department.saveDepartmentToDatabase(main);
-    })
+    const response = await inquirer.prompt(addDepartmentquestions);
 
+    console.log(response);
+
+    const department = new Department(response.name);
+    department.setId( await department.saveDepartmentToDatabase());
+
+    department.displayDepartment();
+
+    main();
+
+}
+
+const viewDepartments = async () => {
+    const list = await new Department().getDepartments();
+    console.table(list);
+    main();
 }
