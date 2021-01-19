39b89b85203f1d2554a8da9f1d267cb2e4a10ef4
Drantho <drantho@gmail.com>
2021-01-18 17:33:07 -0800

    add role functionality added

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 const inquirer = require("inquirer");
 const Department = require("./lib/Department");
+const Role = require("./lib/Role");
 
 const mainQuestions = [
     {
         "name": "action",
         "type": "list",
         "message": "What would you like to do?",
-        "choices": ["Add Department", "View Departments", "Quit"]
+        "choices": ["Add Department", "View Departments", "Add Role", "Quit"]
     }
 ]
 
 const addDepartmentquestions = [
     {
         "name": "name",
-        "type": "type",
+        "type": "input",
         "message": "Enter department name: "
     }
 ]
 
-const main = () => { 
+const main = () => {
 
     inquirer.prompt(mainQuestions).then(response => {
-        switch(response.action){
+        switch (response.action) {
             case "Add Department":
-                addDepartment();                
-                break;
-            case "View Departments" :
+                addDepartment();
+                return;
+            case "View Departments":
                 viewDepartments();
-                break;
-            default :
+                return;
+            case "Add Role":
+                addRole();
+                return;
+            default:
                 return;
         }
     })
@@ -36,14 +40,14 @@ const main = () => {
 
 main();
 
-const addDepartment = async () =>{
+const addDepartment = async () => {
 
     const response = await inquirer.prompt(addDepartmentquestions);
 
     console.log(response);
 
     const department = new Department(response.name);
-    department.setId( await department.saveDepartmentToDatabase());
+    department.setId(await department.saveDepartmentToDatabase());
 
     department.displayDepartment();
 
@@ -55,4 +59,42 @@ const viewDepartments = async () => {
     const list = await new Department().getDepartments();
     console.table(list);
     main();
+}
+
+const addRole = async () => {
+    const list = await new Department().getDepartments();
+    
+    const addRoleQuestions = [
+        {
+            "name": "title",
+            "type": "input",
+            "message": "Enter role title: "
+        },
+        {
+            "name": "salary",
+            "type": "input",
+            "message": "Enter role salary: "
+        },
+        {
+            "name": "department",
+            "type": "list",
+            "message": "Select role department: ",
+            "choices": list
+        }
+    ]
+
+    
+    inquirer.prompt(addRoleQuestions).then(async response => {
+
+        const department = await Department.getDepartmentByName(response.department);
+
+        const role = new Role(response.title, response.salary, department);
+
+        role.setId(await role.saveRoleToDatabase());
+
+        role.displayRole();
+
+        main();
+
+    })
 }
