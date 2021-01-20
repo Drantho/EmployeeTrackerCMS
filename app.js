174f7b990f918326b4d95dab0b8462591358a610
Drantho <drantho@gmail.com>
2021-01-19 23:34:17 -0800

    add employee, updates

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const inquirer = require("inquirer");
 const connection = require("./lib/Database");
 const Department = require("./lib/Department");
 const Role = require("./lib/Role");
+const Employee = require("./lib/Employee");
+const { viewEmployees } = require("./lib/Employee");
 
 const mainQuestions = [
     {
         "name": "action",
         "type": "list",
         "message": "What would you like to do?",
-        "choices": ["Add Department", "View Departments", "Add Role", "View Roles", "Quit"]
+        "choices": ["Add Department", "View Departments", "Update Department", "Add Role", "View Roles", "Add Employee", "View Employees", "Quit"]
     }
 ]
 
@@ -28,16 +30,24 @@ const main = () => {
                 addDepartment();
                 return;
             case "View Departments":
-                viewDepartments();
+                Department.viewDepartments().then(() => main());
                 return;
+            case "Update Department":
+                updateDepartment();
+                break;
             case "Add Role":
                 addRole();
                 return;
             case "View Roles":
                 Role.viewRoles().then(()=>{main()});
                 return;
+            case "Add Employee" :
+                addEmployee();
+                return;
+            case "View Employees" :
+                Employee.viewEmployees().then(()=>main())
             default:
-                connection.end();
+                // connection.end();
                 return;
         }
     })
@@ -60,14 +70,38 @@ const addDepartment = async () => {
 
 }
 
-const viewDepartments = async () => {
-    const list = await new Department().getDepartments();
-    console.table(list);
-    main();
+const updateDepartment = async () =>{
+    const departments = await Department.getDepartments();
+
+    const updateDepartmentQuestions = [
+        {
+            "name": "department",
+            "type": "list",
+            "message": "Select a department to edit",
+            "choices": departments
+        },
+        {
+            "name": "name",
+            "type": "input",
+            "message": "Enter new department name: "
+        }
+    ];
+
+    inquirer.prompt(updateDepartmentQuestions).then(response => {
+        console.log(`response: `, response);
+        connection.query("UPDATE department SET ? WHERE ?", [{department_name: response.name}, {id: response.department.id}], (err, result) => {
+            if(err){
+                throw err
+            }
+
+            console.log(`Department id: ${response.department.id} updated to ${response.name}.`);
+            main();
+        })
+    });
 }
 
 const addRole = async () => {
-    const list = await new Department().getDepartments();
+    const list = await Department.getDepartments();
     
     const addRoleQuestions = [
         {
@@ -91,13 +125,60 @@ const addRole = async () => {
     
     inquirer.prompt(addRoleQuestions).then(async response => {
 
-        const department = await Department.getDepartmentByName(response.department);
+        const department = new Department(response.department.department_name, response.department.id);
 
         const role = new Role(response.title, response.salary, department);
 
         role.setId(await role.saveRoleToDatabase());
 
         role.displayRole();
+
+        main();
+
+    })
+}
+
+const addEmployee = async () => {
+    const roles = await Role.getRoles();
+    const employees = await Employee.getEmployees();
+    employees.push({name: "none", values: {id: 0}});
+
+    const addEmployeeQuestions = [
+        {
+            "name": "firstName",
+            "type": "input",
+            "message": "Enter first name: "
+        },
+        {
+            "name": "lastName",
+            "type": "input",
+            "message": "Enter Last Name: "
+        },
+        {
+            "name": "role",
+            "type": "list",
+            "message": "Select role: ",
+            "choices": roles
+        },
+        {
+            "name": "manager",
+            "type": "list",
+            "message": "Select a manager",
+            "choices": employees
+        }
+    ];
+
+    inquirer.prompt(addEmployeeQuestions).then(async response => {
+
+        const department = new Department( response.role.department, response.role.department_id);
+        const role = new Role(response.role.title, response.role.salary, department, response.role.role_id);
+        const manager = new Employee(response.manager.first_name, response.manager.last_name, {}, 0, response.manager.id || 0)
+        
+        const employee = new Employee(response.firstName, response.lastName, role, manager);
+
+        employee.setId(await employee.saveEmployeeToDatabase());
+
+        employee.displayEmployee();
 
         main();
 
